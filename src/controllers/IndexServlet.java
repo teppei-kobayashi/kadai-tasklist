6c54ca328afc27d459a366e4d0cc7454c3e1a446
teppei.kobayashi <teppe0527@gmail.com>
2020-08-23 23:51:09 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,9 +37,13 @@ public class IndexServlet extends HttpServlet {
 
         List<Tasklist> tasklists = em.createNamedQuery("getAllTasklists", Tasklist.class)
                                    .getResultList();
-        response.getWriter().append(Integer.valueOf(tasklists.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasklists", tasklists);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/index.jsp");
+        rd.forward(request, response);
     }
 
 }
